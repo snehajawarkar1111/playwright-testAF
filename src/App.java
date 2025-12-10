5c4fc430311fa000b08b7ed82100b906e7c766ab
snehajawarkar1111 <snehajawarkar11.11@gmail.com>
2025-12-10 11:53:54 +0530

    new line added to the feature1

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Welcome to the Java Application with feature1 branch.");
+        System.out.println("new line added in feature1 branch.");
     }
 }
